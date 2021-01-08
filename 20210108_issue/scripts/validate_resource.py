6f5ecd4fc2d5d39c21bc1a974f9defee487dc3e6
Ethan Ho <eho@tacc.utexas.edu>
2021-01-08 14:28:15 -0700

    Update test case
    Former-commit-id: b7f23a2d53e4ef3d3ccb23fe80c1ec51e857b608

--- 20210108_issue/scripts/validate_resource.py
+++ 20210108_issue/scripts/validate_resource.py
@@ -1,12 +1,13 @@
+from pprint import pprint as pp
 import frictionless as fl
 
 
 def main():
-    report = fl.validate_resource('dataresource.json')
-    if report.errors:
-        print(report)
-    else:
+    report = fl.validate('dataresource.json')
+    if report.valid:
         print("Is valid")
+    else:
+        pp(report)
 
 
 if __name__ == '__main__':
